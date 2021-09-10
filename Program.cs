e99f1419c1cb2872b381f348c020079a65f890a2
winsermcr <winsermcr@gmail.com>
2021-09-10 17:22:40 +0800

    ok

--- Program.cs
+++ Program.cs
@@ -9,28 +9,37 @@ namespace String_Reduction20210910
     class Program
     {
 
-
-
-
-
-
         // Returns smallest possible length with given
         // operation allowed.
         static int stringReduction(String str)
         {
             int n = str.Length;
 
-            // Counint occurrences of three different
+            // Count occurrences of three different
             // characters 'a', 'b' and 'c' in str
             int[] count = new int[3];
             for (int i = 0; i < n; ++i)
             {
-                count[str[i] - 'a']++;
+
+                //count[str[i] - 'a']++;
+
+
+
+
+                int strI = str[i];
+
+                int strA = 'a';
+
+                int countI = strI - strA;
+
+
+                count[countI] = count[countI] + 1;      //count[countI] += 1;   ==     //count[countI]++;
+
+
             }
 
             // If all characters are same.
-            if (count[0] == n || count[1] == n
-                    || count[2] == n)
+            if (count[0] == n || count[1] == n || count[2] == n)
             {
                 return n;
             }
@@ -38,8 +47,7 @@ namespace String_Reduction20210910
             // If all characters are present even number
             // of times or all are present odd number of
             // times.
-            if ((count[0] % 2) == (count[1] % 2)
-                    && (count[1] % 2) == (count[2] % 2))
+            if ((count[0] % 2) == (count[1] % 2) && (count[1] % 2) == (count[2] % 2))
             {
                 return 2;
             }
@@ -57,9 +65,48 @@ namespace String_Reduction20210910
             String str = "abcbbaacb"; //  abcbbaacb = ccbbaacb = cabaacb = bbaacb = bcacb = aacb = abb = cb = a
             Console.WriteLine(stringReduction(str));
 
+            String str01 = "abcabc"; //  Input: "abcabc"  abcabc      ccabc   cbbc   abc  cc
+            Console.WriteLine(stringReduction(str01));
+
+            String str02 = "cccccccccccccccccccccccccc"; //  Input: "abcabc"  abcabc      ccabc   cbbc   abc  cc
+            Console.WriteLine(stringReduction(str02));
 
 
+            String str03 = "abababababbbbaabbabbbbbcccaaaaabbcc"; //  Input: "abcabc"  abcabc      ccabc   cbbc   abc  cc
+            Console.WriteLine(stringReduction(str03));
 
+
+            String str04 = "caaaaaaabbbcabbbbbbc"; //  Input: "abcabc"  abcabc      ccabc   cbbc   abc  cc
+            Console.WriteLine(stringReduction(str04));
+
+
+            String str05 = "ccbbbbbbbbbbbbbbbbbbbbbbbbbbbc"; //  Input: "abcabc"  abcabc      ccabc   cbbc   abc  cc
+            Console.WriteLine(stringReduction(str05));
+
+
+            String str06 = "ccbaaaaaaaaaaaaaaaaabbbcc"; //  Input: "abcabc"  abcabc      ccabc   cbbc   abc  cc
+            Console.WriteLine(stringReduction(str06));
+
+
+            String str07 = "ccccbbbbbbbbbbbbbbbbbbbaaaaaaaaa"; //  Input: "abcabc"  abcabc      ccabc   cbbc   abc  cc
+            Console.WriteLine(stringReduction(str07));
+
+            String str08 = "ccccaaabbbababaababbaa"; //  Input: "abcabc"  abcabc      ccabc   cbbc   abc  cc
+            Console.WriteLine(stringReduction(str08));
+
+
+            String str09 = "aaaaaaaaaaaaaaaabbbbbbbaaaaaaaabbbabbababccccccccbbccccaccccb"; //  Input: "abcabc"  abcabc      ccabc   cbbc   abc  cc
+            Console.WriteLine(stringReduction(str09));
+
+
+            String str10 = "cccaaaaaaaaaaaaa"; //  Input: "abcabc"  abcabc      ccabc   cbbc   abc  cc
+            Console.WriteLine(stringReduction(str10));
+
+            String str11 = "cbbbbbbbbbbbbbbbccccccccccccccaaaaaaaaaccc"; //  Input: "abcabc"  abcabc      ccabc   cbbc   abc  cc
+            Console.WriteLine(stringReduction(str11));
+
+            String str12 = "ccccaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbb"; //  Input: "abcabc"  abcabc      ccabc   cbbc   abc  cc
+            Console.WriteLine(stringReduction(str12));
 
             Console.ReadLine();
 
